357173ba46d2cecd11bb0d78a014cde4d2ad040c
Gabriel Bohn <gabrielcbohn@gmail.com>
2025-03-25 15:51:55 -0300

    fix: tailwind integration with nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
     content: ['./app/**/*.{js,jsx,ts,tsx}'],
+    presets: [require("nativewind/preset")],
     theme: {
         extend: {
             colors: {
