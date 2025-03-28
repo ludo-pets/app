124afdfc2616cc0ae61f29d0b639b6bfedab0879
PedroRamos02 <phramosaraujo02@gmail.com>
2025-03-28 18:13:30 -0300

    Componente estatico finalizado

--- components/XpBar.tsx
+++ components/XpBar.tsx
@@ -2,18 +2,29 @@ import React, { useEffect, useState, useRef } from "react";
 import { View, Text, StyleSheet, Animated, Image } from "react-native";
 
 const XpBar = () => {
-  const [xp, setXp] = useState(0); 
+  const [xp, setXp] = useState(0);
+  const [level, setLevel] = useState(1); 
   const animatedValue = useRef(new Animated.Value(0)).current;
 
-  
   const barWidth = animatedValue.interpolate({
     inputRange: [0, 100],
     outputRange: ["0%", "100%"],
   });
 
+  useEffect(() => {
+    if (xp >= 100) {
+      setLevel((prev) => prev + 1);
+      setXp(0); 
+      animatedValue.setValue(0); 
+    }
+  }, [xp]);
+
   return (
     <View style={styles.container}>
-      <Image style={styles.img} source={require('../assets/images/paw-xp-bar.png')}/>
+      <View style={styles.pawContainer}>
+        <Image source={require("../assets/images/paw-xp-bar.png")} />
+        <Text style={styles.levelText}>{level}</Text>
+      </View>
       <View style={styles.bar}>
         <Text style={styles.text}>{xp}/100</Text>
         <View style={styles.barBackground}>
@@ -26,22 +37,33 @@ const XpBar = () => {
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
     flexDirection: "row",
     width: "90%",
+    alignItems: "center",
     marginHorizontal: "auto",
   },
-  img: {
+  pawContainer: {
+    position: "relative", 
     marginRight: 10,
   },
+  levelText: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -4 }, { translateY: -3.4 }],
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#5B5B5B",
+  },
   text: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: "bold",
     marginBottom: 5,
     alignSelf: "flex-end",
+    color: "#5B5B5B",
   },
   bar: {
-    width: 270
+    width: 260,
   },
   barBackground: {
     width: "100%",
