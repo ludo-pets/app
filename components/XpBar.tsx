002d802c992a14646074dc9b8c45890e5cbb7e02
PedroRamos02 <phramosaraujo02@gmail.com>
2025-03-28 13:38:47 -0300

    Barra de Xp adicionada

--- components/XpBar.tsx
+++ components/XpBar.tsx
@@ -1,7 +1,49 @@
-const XpBar = () => {
-    return(
-        <></>
-    )
-}
+import React, { useEffect, useState, useRef } from "react";
+import { View, Text, StyleSheet, Animated } from "react-native";
 
-export default XpBar; 
+const XpBar = () => {
+  const [xp, setXp] = useState(0); 
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  
+  const barWidth = animatedValue.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"],
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{xp}/100</Text>
+      <View style={styles.barBackground}>
+        <Animated.View style={[styles.barFill, { width: barWidth }]} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    width: "90%",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+    alignSelf: "flex-end",
+  },
+  barBackground: {
+    width: "100%",
+    height: 20,
+    backgroundColor: "#ddd",
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  barFill: {
+    height: "100%",
+    backgroundColor: "#80BEE7",
+  },
+});
+
+export default XpBar;
