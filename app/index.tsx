31ddabe4500bbb1f23acdbf5ac76e6eca7ef7f93
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2025-04-01 23:08:42 -0300

    adds the index file

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import { Route } from "expo-router";
 import Minigame from "./minigame";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
 import NailTrimGame from "@/components/minigames/nail-trim-game";
 
 const Stack = createNativeStackNavigator();
