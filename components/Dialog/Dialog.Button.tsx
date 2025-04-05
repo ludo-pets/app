392bfb1cec5bce147268b21954a27578b7c28f72
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2025-04-05 19:54:43 -0300

    feat(minigame): creating the trimming nail minigame

---
+++ components/Dialog/Dialog.Button.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+export type DialogButtonProps = {
+    action: () => void,
+    text: string,
+    color?: string,
+    background?: string,
+};
+
+export default function DialogButton({action, text, color = "#5B5B5B", background = "#CFE2A8"}:DialogButtonProps ){
+    return(
+        <TouchableOpacity
+            style={{...style.dialogButton, backgroundColor: background}}
+            onPress={action}
+        >
+            <Text style={{...style.buttonText, color}}>{text}</Text>
+        </TouchableOpacity>
+    );
+}
+const style = StyleSheet.create({
+    dialogButton: {
+        backgroundColor: "#D9E8B9",
+        padding: 10,
+        borderRadius: 10,
+        boxShadow: "2px 2px 4px 0px rgba(0,0,0,0.2)",
+        alignItems: "center",
+        width: "60%",
+        fontFamily: "Roboto",
+    },
+    buttonText: {
+        fontFamily: "Roboto",
+        fontSize: 25,
+        fontWeight: 500,
+        color: "#5B5B5B",
+    }
+})
---
+++ components/minigames/nail-trim-game/StatGameDialog.tsx
@@ -0,0 +1,19 @@
+import { Animated, Dimensions, StyleSheet, Text, TouchableOpacity } from "react-native";
+import Dialog from "@/components/Dialog/Dialog";
+
+const { width, height } = Dimensions.get("window");
+export default function StartGameDialog({startGame}: {startGame: (value: boolean) => void}){
+    return(
+        <Dialog.Container> 
+            <Dialog.Text
+            content="Arraste o cortador para cima das unhas do seu pet para cortá-las"
+            />
+            <Dialog.ButtonArea>
+                <Dialog.Button
+                    action={() => startGame(true)}
+                    text="Começar"
+                />
+            </Dialog.ButtonArea>
+        </Dialog.Container>
+    )
+}
