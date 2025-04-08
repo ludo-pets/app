a0a8207f200ecf66c55baa0bc1e1d2bbc8d35f3b
Gabriel Bohn <gabrielcbohn@gmail.com>
2025-04-08 11:34:12 -0300

    fix: minigame missing imports and some file names

--- components/minigames/nail-trim-game/StatGameDialog.tsx
+++ components/minigames/nail-trim-game/StatGameDialog.tsx
@@ -1,18 +1,15 @@
-import { Animated, Dimensions, StyleSheet, Text, TouchableOpacity } from "react-native";
-import Dialog from "@/components/Dialog/Dialog";
+import Dialog from '@/components/Dialog/Dialog'
 
-const { width, height } = Dimensions.get("window");
-export default function StartGameDialog({startGame}: {startGame: (value: boolean) => void}){
-    return(
-        <Dialog.Container> 
-            <Dialog.Text
-            content="Arraste o cortador para cima das unhas do seu pet para cortá-las"
-            />
+export default function StartGameDialog({
+    startGame,
+}: {
+    startGame: (value: boolean) => void
+}) {
+    return (
+        <Dialog.Container>
+            <Dialog.Text content="Arraste o cortador para cima das unhas do seu pet para cortá-las" />
             <Dialog.ButtonArea>
-                <Dialog.Button
-                    action={() => startGame(true)}
-                    text="Começar"
-                />
+                <Dialog.Button action={() => startGame(true)} text="Começar" />
             </Dialog.ButtonArea>
         </Dialog.Container>
     )
