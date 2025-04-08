45302f19f3ae26504f2dfe81d4850069f4477498
Erik Vieira Suris <erik.suris@edu.pucrs.br>
2025-04-08 00:31:18 -0300

    fix: fixing entities types

--- dtos/Pet.ts
+++ dtos/Pet.ts
@@ -6,12 +6,12 @@ export default interface Pet {
     purchasedItems: {
         id: string;
         quantity?: number;
-    };
+    }[];
     activeItems: string[];
     wellBeing : {
-        clean: number;
-        fun: number;
-        hunger: number;
-        thirst: number;
+        clean: Date;
+        fun: Date;
+        hunger: Date;
+        thirst: Date;
     };
 }
