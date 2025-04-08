7cd52aab509f6adf79cf1d9c14d9f6af8a9d4a59
Matheus Bueno de Oliveira <matheus.060@edu.pucrs.br>
2025-04-08 04:16:28 +0000

    Feat/29 consume data firebase pet store

--- app/(tabs)/quiz.tsx
+++ app/(tabs)/quiz.tsx
@@ -1,10 +1,29 @@
-import { StyleSheet, View, Text } from 'react-native'
+import { useUserPetStore } from '@/stores/ludoStore'
+import { StyleSheet, View, Text, Button } from 'react-native'
 
 export default function QuizScreen() {
+    const user = useUserPetStore((state) => state.user)
+    const userUpdate = useUserPetStore((state) => state.updateUser)
+    const petUpdate = useUserPetStore((state) => state.updatePet)
+    const pet = useUserPetStore((state) => state.pet)
+
+    const handleEndQuiz = () => {
+        console.log('Quiz finalizado')
+        if (!user) return
+        userUpdate(user?.id || '', {
+          money: user?.money + 10,
+          experience: user?.experience + 10,
+        })
+        petUpdate(pet?.id || '', {
+          name: 'Alaor',
+        })
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Quiz</Text>
             <View style={styles.separator} />
+            <Button title="Finalizar Quiz" onPress={handleEndQuiz} />
         </View>
     )
 }
