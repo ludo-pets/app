f82fbd51e5baf599ce1ee5be515dd955928f95c9
Luiz <kubaszewski001@hotmail.com>
2025-04-18 22:40:50 -0300

    feat/60/Creating interface itemStore and fetch logic.

---
+++ stores/itemStore.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand'
+import Item from '@/dtos/Item'
+
+import { getAllItemsService } from '@/services/itemService'
+
+interface ItemStoreState {
+    items: Item[]
+    loading: boolean
+    error: string | null
+    fetchItems: () => Promise<void>
+    setItems: (items : Item[]) => void
+}
+
+export const useItemStore = create<ItemStoreState>((set) => ({
+    items: [],
+    loading: false,
+    error:null,
+
+    fetchItems: async () => {
+        set({loading: true, error: null})
+        try{
+            const result = await getAllItemsService()
+            if(result) {
+                set({ items: result.items, loading: false})
+            } else {
+                set({ error: 'Itens não encontrados', loading: false})
+            }
+        } catch(error: any) {
+            set({ error: error.message || 'Erro ao buscar itens', loading: false})
+        }
+    },
+
+        setItems: (items: Item[]) => set({items}),
+}))
