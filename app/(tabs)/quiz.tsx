8ed2115e48ca22902fb8e5ac81844b4cdd2d740a
Pedro Augusto Pereira <pereiraaugustopedro2021@gmail.com>
2025-04-24 17:35:50 -0300

    feat/47-Criar-tela-de-progresso

--- app/(tabs)/quiz.tsx
+++ app/(tabs)/quiz.tsx
@@ -1,28 +1,80 @@
+import React from 'react'
+import { StyleSheet, View, Text, ScrollView, SafeAreaView, Image, Dimensions } from 'react-native'
+import User from '@/dtos/User';
+import ItemPathQuiz from '@/components/quiz/ItemPathQuiz';
 import { useUserPetStore } from '@/stores/userPetStore'
-import { StyleSheet, View, Text, Button } from 'react-native'
+
+const paws = require('@/assets/images/paw.svg');
+const pawsTravled = require('@/assets/images/pawTraveled.svg');
+interface pathItemQuiz {
+    icon: string,
+    name: string
+    concluded: boolean,
+    id: string
+}
+
+const user : User = {
+    email: 'default@gmail.com',
+    money: 10,
+    level: 1,
+    experience: 10,
+    lastLessonConcluded: '1',
+    notifications: true,
+    pet: 'Simba',
+    id: '1'
+}
+
+const catQuizzes: pathItemQuiz[] = [
+    { icon: 'Medal', name: 'Primeiros cuidados com o gato', concluded: true, id: '0' },
+    { icon: 'BowlFood', name: 'Nutrição e alimentação adequada', concluded: false, id: '1' },
+    { icon: 'Toilet', name: 'Caixa de areia: uso e limpeza', concluded: false, id: '2' },
+    { icon: 'HandHeart', name: 'Construindo confiança com seu gato', concluded: false, id: '3' },
+    { icon: 'PuzzlePiece', name: 'Estimulação mental e brinquedos', concluded: false, id: '4' },
+    { icon: 'Syringe', name: 'Vacinas e cuidados de saúde', concluded: false, id: '5' },
+    { icon: 'Dresser', name: 'Organização do espaço do gato', concluded: false, id: '6' },
+    { icon: 'ShieldCheck', name: 'Prevenindo acidentes em casa', concluded: false, id: '7' },
+    { icon: 'Scissors', name: 'Cuidados com unhas e pelos', concluded: false, id: '8' },
+    { icon: 'Heartbeat', name: 'Sinais de estresse e dor', concluded: false, id: '9' },
+    { icon: 'Bird', name: 'Gatos e caça: instintos naturais', concluded: false, id: '10' },
+    { icon: 'MoonStars', name: 'Gatos noturnos: como lidar?', concluded: false, id: '11' },
+    { icon: 'HandsClapping', name: 'Gato e visitas: como apresentar?', concluded: false, id: '12' },
+    { icon: 'Thermometer', name: 'Temperatura ideal e cuidados com o clima', concluded: false, id: '13' },
+    { icon: 'MapPin', name: 'Como transportar o gato com segurança', concluded: false, id: '14' },
+    { icon: 'Plant', name: 'Plantas perigosas para gatos', concluded: false, id: '15' },
+    {
+        icon: 'Trophy',
+        name: 'Desafio final: cuidados com seu pet',
+        concluded: false,
+        id: 'final'
+    }
+  ];
 
 export default function QuizScreen() {
-    const user = useUserPetStore((state) => state.user)
-    const userUpdate = useUserPetStore((state) => state.updateUser)
-    const petUpdate = useUserPetStore((state) => state.updatePet)
-    const pet = useUserPetStore((state) => state.pet)
+    const userUpdate = useUserPetStore((state: { updateUser: any; }) => state.updateUser)
+    const petUpdate = useUserPetStore((state: { updatePet: any; }) => state.updatePet)
+    const pet = useUserPetStore((state: { pet: any; }) => state.pet)
 
-    const handleEndQuiz = () => {
-        console.log('Quiz finalizado')
-        if (!user) return
-        userUpdate(user?.id || '', {
-            money: user?.money + 10,
-            experience: user?.experience + 10,
-        })
-        petUpdate(pet?.id || '', {
-            name: 'Alaor',
-        })
+    const OnPressItem = (id : string) => {
+        //Move para tela do quiz
     }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Quiz</Text>
-            <View style={styles.separator} />
+            <SafeAreaView style={{ flex: 1, width: '100%', height: '100%' }}>
+                <ScrollView style={{ flex: 1, width: '100%', height: '100%' }} contentContainerStyle={{ paddingBottom: 20 }}>
+                    {catQuizzes.map((item, index) => (
+                        <> 
+                            <ItemPathQuiz pendent={user.lastLessonConcluded == item.id} onPress={() => OnPressItem(item.id)} id={item.id} name={item.name} index={index} icon={item.icon} concluded={item.concluded} />
+                            {catQuizzes.length - 1 !== index && (
+                                <View style={styles.boxPaws}>
+                                    <Image source={item.concluded === false ? paws : pawsTravled} />
+                                    <Image source={item.concluded === false ? paws : pawsTravled} />
+                                </View>
+                            )}    
+                        </>        
+                    ))}
+                </ScrollView>
+            </SafeAreaView>
         </View>
     )
 }
@@ -32,14 +84,18 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        height: '100%',
     },
     title: {
         fontSize: 20,
         fontWeight: 'bold',
     },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
+    boxPaws :{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20,
+        marginTop: 5,
+        marginBottom: 7,
+    }
 })
