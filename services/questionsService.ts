9499de83874105b7313bd9f105302e0fbbafba54
Luiza Heller <luiza.heller@edu.pucrs.br>
2025-05-08 21:53:37 -0300

    feat: creating fetchQuestion method

--- services/questionsService.ts
+++ services/questionsService.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore'
 import { db } from '@/firebaseConfig'
 import Question from '@/dtos/Question'
 
@@ -24,5 +24,30 @@ export const fetchAllQuestionsService = async (): Promise<{
         return { questions }
     } catch (error) {
         throw new Error('Failed to fetch quiz questions.')
+    }
+}
+
+export const fetchQuestion = async(questionId: string): Promise<Question> => {
+    try {
+        const questionDocRef = doc(db, 'Question', questionId)
+        const questionSnapshot = await getDoc(questionDocRef)
+
+        if (questionSnapshot.exists()) {
+            const data = questionSnapshot.data()
+            return {
+                id: questionSnapshot.id,
+                answers: data.answers || [],
+                description: data.description || 'No description',
+                explanation: data.explanation || 'No explanation',
+                // image: data.image,
+                rightAnswer: data.rightAnswer,
+                title: data.title || 'Untitled Question',
+            } as Question
+        } else {
+            throw new Error(`Question with ID ${questionId} does not exist.`)
+        }
+    } catch (error) {
+        console.error('Error fetching question:', error)
+        throw new Error('Failed to fetch the question.')
     }
 }
