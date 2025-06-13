f4797ee72ce8e4e7dc295465e00fce5bd2cf823e
Luiza Heller <luiza.heller@edu.pucrs.br>
2025-06-13 20:35:31 -0300

    feat: game styling

--- components/minigames/pets-2048/AppScreen.tsx
+++ components/minigames/pets-2048/AppScreen.tsx
@@ -1,48 +1,62 @@
-import { View, Text, StyleSheet, Button } from "react-native";
-import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import Board from "./Board";
-import { theme } from "../../../constants/minigames/Pets2048";
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import Board from './Board'
+import { theme } from '../../../constants/minigames/Pets2048'
 
 const AppScreen = () => {
-  const [resetKey, setResetKey] = useState(0);
+    const [resetKey, setResetKey] = useState(0)
 
-  const handleNewGame = () => {
-    setResetKey(prev => prev + 1);
-  };
+    const handleNewGame = () => {
+        setResetKey((prev) => prev + 1)
+    }
 
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.heading}>2048 Pets</Text>
-      </View>
-      {/* Score is now handled inside Board */}
-      <Board resetKey={resetKey} />
-      <View style={styles.footer}>
-        <Button title="Novo Jogo" onPress={handleNewGame} color="#E8A598" />
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
+    return (
+        <View style={styles.container}>
+            <View style={styles.titleBackground}>
+                <Text style={styles.heading}>2048 Pets</Text>
+            </View>
+            <Board resetKey={resetKey} />
+            <View style={styles.newGame}>
+                <Button
+                    title="Novo Jogo"
+                    onPress={handleNewGame}
+                    color="#E8A598"
+                />
+            </View>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FAF8EF",
-    padding: 16,
-    paddingTop: 48,
-    justifyContent: "space-between",
-  },
-  heading: {
-    fontSize: 64,
-    fontWeight: "900",
-    color: "#E8A598",
-    fontFamily: theme.fonts.regular,
-  },
-  footer: {
-    marginBottom: 48,
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#FAF8EF',
+        padding: 16,
+        paddingTop: 48,
+        justifyContent: 'space-between',
+    },
+    titleBackground: {
+        backgroundColor: '#E8A598',
+        width: 200,
+        borderRadius: 10,
+        alignItems: "center"
+    },
+    heading: {
+        fontSize: 40,
+        fontWeight: '900',
+        color: 'white',
+        fontFamily: theme.fonts.regular,
+    },
+    newGame: {
+        width: 100,
+        marginBottom: 48,
+        marginRight: 10,
+        alignSelf: 'flex-end',
+    },
+})
 
-export default AppScreen;
+
+
+export default AppScreen
